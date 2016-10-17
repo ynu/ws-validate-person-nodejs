6fdb0705395cb6a08e9d330774f5e827637eae90
liudonghua123 <liudonghua123@gmail.com>
2016-10-17 15:41:17 +0800

    fix single PERSON_INFO issue

--- soap-server.js
+++ soap-server.js
@@ -2,13 +2,13 @@ var http=require('http');
 var soap = require('soap');
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
-var builder = new xml2js.Builder();
+var builder = new xml2js.Builder({cdata: true});
 
 var myService = {
     validatePerson: {
         validatePerson_Port: {
             validatePerson: function(person) {
-                console.info("%j", person);
+                console.info("parsed request is %j", person);
                 var rtn_person = {
                     RTN_PERSON: {
                         ERR_ID: 'OK',
@@ -18,12 +18,19 @@ var myService = {
                 }
                 person_infos = person.person.$value.PERSON.PERSON_INFO_LIST.PERSON_INFO;
                 rtn_person_infos = rtn_person.RTN_PERSON.RTN_PERSON_INFO_LIST.RTN_PERSON_INFO;
-                for(var i = 0; i < person_infos.length; i++) {
-                    person_info = person_infos[i];
-                    rtn_person_infos.push({SORT_ID: person_info.SORT_ID,PERSON_TYPE: '1'});
+                if(person_infos.constructor === Array) {
+                    for(var i = 0; i < person_infos.length; i++) {
+                        person_info = person_infos[i];
+                        rtn_person_infos.push({SORT_ID: person_info.SORT_ID,PERSON_TYPE: '1'});
+                    }
                 }
-                var rtn_person_string = "<![CDATA[" + builder.buildObject(rtn_person) + "]]>";
-                return {validatePersonResult: rtn_person_string};
+                else if(typeof person_infos === "object") {
+                    rtn_person_infos.push({SORT_ID: rtn_person_infos.SORT_ID,PERSON_TYPE: '1'});
+                }
+                var rtn_person_string = builder.buildObject(rtn_person);
+                var rtn_person_cdata_string = "<![CDATA[" + rtn_person_string + "]]>";
+                console.info("parsed response is %s", rtn_person_cdata_string);
+                return {validatePersonResult: rtn_person_cdata_string};
             }
         }
     }
